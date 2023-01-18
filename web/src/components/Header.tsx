ae026fce5cd8d62c2e1dbffc917ba33f33e6142b
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2023-01-18 14:01:38 -0300

    feat: first view of site

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
         type="button"
         className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
       >
-        Novo Hábito
+        New Habit
         <Plus size={20} className="text-violet-500" />
       </button>
     </div>
---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,44 @@
+import { datesFromYearBeginning } from "../utils/datesFromYearBeginning";
+import { HabitDay } from "./HabitDay";
+
+const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const sumaryDates = datesFromYearBeginning();
+const minimumSumaryDates = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSumaryDates - sumaryDates.length;
+
+const SummaryTable: React.FC = () => {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((day) => {
+          return (
+            <div
+              key={day}
+              className="text-zinc-400 text-xl h-10 w-10 mr-4 flex items-center justify-end"
+            >
+              {day}
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {sumaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default SummaryTable;
