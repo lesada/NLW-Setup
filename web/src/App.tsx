ed16204fadf1148ad9a1d2a10a342db65c233efe
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2023-01-18 11:44:01 -0300

    feat: header web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
+import Header from "./components/Header";
 import "./styles/global.css";
-import { Habit } from "./components/Habit";
 
 function App() {
   return (
-    <div className="App">
-      <Habit completed={5} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
     </div>
   );
 }
