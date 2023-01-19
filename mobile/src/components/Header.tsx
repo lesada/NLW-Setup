2e6bc2b26f89910c8efce72d3a0107f7bed90612
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2023-01-19 17:52:44 -0300

    wip: creating routes

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,14 +1,20 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
+import { useNavigation } from "@react-navigation/native";
 
 import Logo from "../assets/logo.svg";
 
 const components: React.FC = () => {
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-fulll flex-row items-center justify-between">
       <Logo />
-      <TouchableOpacity className="flex-row items-center h-11 px-4 border border-violet-500 rounded-lg">
+      <TouchableOpacity
+        className="flex-row items-center h-11 px-4 border border-violet-500 rounded-lg"
+        onPress={() => navigate("new")}
+      >
         <Feather name="plus" color={colors.violet[500]} size={20} />
         <Text className="text-white ml-3 font-semibold text-base">New</Text>
       </TouchableOpacity>
